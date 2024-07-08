6351b40450ee01fd4132bc14307414ecca16113b
Nargiza <nargizh03@gmail.com>
2024-07-08 19:57:28 +0500

    fix bugs

--- src/main/java/com/lorby/auth_project/service/EmailService.java
+++ src/main/java/com/lorby/auth_project/service/EmailService.java
@@ -7,7 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
 import org.springframework.mail.MailSendException;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -23,23 +25,16 @@ public class EmailService {
     @Value("${EMAIL_USERNAME}")
     private String from;
     public void sendEmail(String to, String subject, String text){
-        logger.debug("EMAIL_USERNAME: {}", System.getenv("EMAIL_USERNAME"));
-        logger.debug("EMAIL_PASSWORD: {}", System.getenv("EMAIL_PASSWORD"));
         try{
-            logger.info("Sending email to: {}", to);
-            MimeMessage message = emailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setFrom(from);
-            helper.setTo(to);
-            helper.setSubject(subject);
-            helper.setText(text, true);
-            logger.debug("Preparing to send email to: {}", to);
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(from);
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            log.debug("Preparing to send email to: {}", to);
             emailSender.send(message);
-            log.info("Email sent to: {}", to);
-            logger.info("Email sent successfully to: {}", to);
-        } catch (MessagingException ex){
+        } catch (MailException ex){
             logger.error("Failed to send email to: {}", to, ex);
-            ex.printStackTrace();
             throw new MailSendException("Failed to send email");
         }
     }
